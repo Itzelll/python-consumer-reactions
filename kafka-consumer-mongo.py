6e550437f3644d188a3d0d0ad31404d68cd2180d
itzelll <mmitzel2435@gmail.com>
2023-05-29 22:54:02 -0600

    reactions summary

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -9,26 +9,26 @@ import json
 import subprocess
 
 
-# replace here with your mongodb url 
+# replace here with your mongodb url
 uri = "mongodb+srv://itzelll:NtWMhS9DNb1RzPp0@comidas.tv394y9.mongodb.net/?retryWrites=true&w=majority"
-#uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2"
+# uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2"
 
 
 # Create a new client and connect to the server
-#client = MongoClient(uri, server_api=ServerApi('1'))
+# client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 
-#try:
+# try:
 #    client.admin.command('ping')
 #    print("Pinged your deployment. You successfully connected to MongoDB!")
-#except Exception as e:
+# except Exception as e:
 #    print(e)
 
 # Connect to MongoDB and pizza_data database
 
 try:
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    client.admin.command('ping')
+    client = MongoClient(uri, server_api=ServerApi("1"))
+    client.admin.command("ping")
     print("Pinged your deployment. You successfully connected to MongoDB!")
 
     db = client.memes
@@ -36,44 +36,51 @@ try:
 except:
     print("Could not connect to MongoDB")
 
-consumer = KafkaConsumer('test',bootstrap_servers=[
-    'comida-kafka-0.comida-kafka-headless.itzel2-itzelll.svc.cluster.local:9092'
-     #'localhost:9092'
-    ])
+consumer = KafkaConsumer(
+    "test",
+    bootstrap_servers=[
+        "comida-kafka-0.comida-kafka-headless.itzel2-itzelll.svc.cluster.local:9092"
+        #'localhost:9092'
+    ],
+)
 # Parse received data from Kafka
 for msg in consumer:
     record = json.loads(msg.value)
     print(record)
-    name = record['name']
+    userId = record["userId"]
+    objectId = record["objectId"]
+    reactionId = record["reactionId"]
 
     # Create dictionary and ingest data into MongoDB
     try:
-       meme_rec = {'name':name }
-       print (meme_rec)
-       meme_id = db.memes_info.insert_one(meme_rec)
-       print("Data inserted with record ids", meme_id)
+        meme_rec = {"userId": userId, "objectId": objectId, "reactionId": reactionId}
+        print(meme_rec)
+        meme_id = db.memes_info.insert_one(meme_rec)
+        print("Data inserted with record ids", meme_id)
 
-       subprocess.call(['sh', './test.sh'])
+        subprocess.call(["sh", "./test.sh"])
 
     except:
-       print("Could not insert into MongoDB")
+        print("Could not insert into MongoDB")
 
-
-# Create dictionary and ingest data into MongoDB
+    # Create dictionary and ingest data into MongoDB
     try:
-       agg_result= db.memes_info.aggregate(
-       [{
-         "$group" : 
-         {  "_id" : "$name", 
-            "n"    : {"$sum": 1}
-         }}
-       ])
-       db.memes_summary.delete_many({})
-       for i in agg_result:
-         print(i)
-         summary_id = db.memes_summary.insert_one(i)
-         print("Summary inserted with record ids", summary_id)
+        agg_result = db.memes_reactions.aggregate(
+            [
+                {
+                    "$group": {
+                        "_id": {"objectId": "$objectId", "reactionId": "$reactionId"},
+                        "n": {"$sum": 1},
+                    }
+                }
+            ]
+        )
+        db.memes_summary.delete_many({})
+        for i in agg_result:
+            print(i)
+            summary_id = db.memes_summary_reactions.insert_one(i)
+            print("Reaction inserted with record ids", summary_id)
 
     except Exception as e:
-       print(f'group by caught {type(e)}: ')
-       print(e)
+        print(f"group by caught {type(e)}: ")
+        print(e)
